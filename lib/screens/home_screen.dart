29d198ba22dd607294c1d3f896d0edc7f84814b9
Einarr07 <aidenperse@gmail.com>
2025-10-01 08:47:09 -0500

    Crating of forms and buttons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,12 +7,35 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[_RecipesCard(context), _RecipesCard(context)],
+        children: <Widget>[
+          _recipesCard(context),
+          _recipesCard(context),
+          _recipesCard(context),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        child: Icon(Icons.add, color: Colors.white),
+        onPressed: () {
+          _showBottom(context);
+        },
       ),
     );
   }
 
-  Widget _RecipesCard(BuildContext context) {
+  Future<void> _showBottom(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      builder: (context) => Container(
+        width: MediaQuery.of(context).size.width,
+        height: 500,
+        color: Colors.white,
+        child: RecipeFrom(),
+      ),
+    );
+  }
+
+  Widget _recipesCard(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
@@ -24,13 +47,12 @@ class HomeScreen extends StatelessWidget {
               Container(
                 height: 125,
                 width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(12),
-                ),
                 child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(),
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    'assets/images/lasagna.png',
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
               SizedBox(width: 26),
@@ -54,6 +76,131 @@ class HomeScreen extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class RecipeFrom extends StatelessWidget {
+  const RecipeFrom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
+    final TextEditingController _recipeName = TextEditingController();
+    final TextEditingController _recipeAuthor = TextEditingController();
+    final TextEditingController _recipeImage = TextEditingController();
+    final TextEditingController _recipeDescription = TextEditingController();
+
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Add New Recipe',
+              style: TextStyle(
+                color: Colors.orange,
+                fontSize: 24,
+                fontFamily: 'Arial',
+              ),
+            ),
+            SizedBox(height: 16),
+            _buildTextField(
+              controller: _recipeName,
+              label: 'Recipe Name',
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the recipe name';
+                }
+                return null;
+              },
+            ),
+            _buildTextField(
+              controller: _recipeAuthor,
+              label: 'Author',
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the author';
+                }
+                return null;
+              },
+            ),
+            _buildTextField(
+              controller: _recipeImage,
+              label: 'Image URL',
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter image url';
+                }
+                return null;
+              },
+            ),
+            _buildTextField(
+              controller: _recipeDescription,
+              label: 'Description',
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the description';
+                }
+                return null;
+              },
+              maxLines: 4,
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pop(context);
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Save Recipe',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Arial',
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Widget personal
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    required String? Function(String?) validator,
+    int maxLines = 1,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(fontFamily: 'Arial', color: Colors.orange),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.orange, width: 1),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        validator: validator,
+        maxLines: maxLines,
       ),
     );
   }
